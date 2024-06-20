6d9c1120af083135981c4e34e74525263311f6c3
Nguyễn Thành Kỷ <146532101+kynguyen060503@users.noreply.github.com>
2024-06-20 20:34:02 +0700

    autocomplete

--- target/classes/static/wwwroot/js/main.js
+++ target/classes/static/wwwroot/js/main.js
@@ -1,38 +1,39 @@
 $(document).ready(function () {
-    $("#search-input").on("keyup", function () {
-        var query = $(this).val();
-        if (query !== '') {
-            $.ajax({
-                url: "/song/SearchSuggestions",
-                type: "GET",
-                data: { query: query },
-                dataType: "json",
-                success: function (data) {
-                    $("#search-results").empty();
-                    $.each(data, function (index, suggestion) {
-                        $("#search-results").append("<div class='suggestion'>" + suggestion + "</div>");
+            $("#search-input").on("keyup", function () {
+                console.log("Key pressed:", event.key);
+                var query = $(this).val();
+                if (query !== '') {
+                    $.ajax({
+                        url: "/books/SearchSuggestions",
+                        type: "GET",
+                        data: { query: query },
+                        dataType: "json",
+                        success: function (data) {
+                            $("#search-results").empty();
+                            $.each(data, function (index, suggestion) {
+                                $("#search-results").append("<div class='suggestion'>" + suggestion + "</div>");
+                            });
+                            $(".dropdown-content").show();
+                        },
+                        error: function (xhr, status, error) {
+                            console.log("Lỗi:", error);
+                        }
                     });
-                    $(".dropdown-content").show();
-                },
-                error: function (xhr, status, error) {
-                    console.log("Lỗi:", error);
+                } else {
+                    $("#search-results").empty();
+                    $(".dropdown-content").hide();
                 }
             });
-        } else {
-            $("#search-results").empty();
-            $(".dropdown-content").hide();
-        }
-    });
-    $(document).on("click", ".suggestion", function () {
-        var selectedSuggestion = $(this).text();
-        $("#search-input").val(selectedSuggestion);
-        $("#search-results").empty();
-        $(".dropdown-content").hide();
-    });
+            $(document).on("click", ".suggestion", function () {
+                var selectedSuggestion = $(this).text();
+                $("#search-input").val(selectedSuggestion);
+                $("#search-results").empty();
+                $(".dropdown-content").hide();
+            });
 
-    $(document).on("click", function (event) {
-        if (!$(event.target).closest('.dropdown').length) {
-            $(".dropdown-content").hide();
-        }
-    });
-});
+            $(document).on("click", function (event) {
+                if (!$(event.target).closest('.dropdown').length) {
+                    $(".dropdown-content").hide();
+                }
+            });
+        });
